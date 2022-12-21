37e0fc2ff5a78441fa4c548b941e13dd8216a67a
psolorzn <psolorzn@yahoo.com>
2022-12-21 05:17:42 -0500

    Update sql-matricula2023.sql

--- sql-matricula2023.sql
+++ sql-matricula2023.sql
@@ -1,1 +1,3 @@
 select a.codmod, b.ie, b.nivel, count(a.codmod) as cantidad from msolicitudes a JOIN matri01 b ON a.codmod = b.codmod group by a.codmod having count(a.codmod)>0
+
+select a.codmod, b.ie, c.nivel, count(a.codmod) as cantidad from msolicitudes a JOIN matri01 b ON a.codmod = b.codmod join niveles c on b.nivel = c.idnivel group by a.codmod having count(a.codmod)>0
